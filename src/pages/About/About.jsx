2ee58f3d9ab734aa3caa78ae1d3d6158eae73755
Brian Ferreira <101906190+EoBrian@users.noreply.github.com>
2022-09-02 10:43:05 -0300

    add post working

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const About = () => {
   return (
     <div className='box'>
       <h2>Mini <span>BLOG</span></h2>
+      
+      <br />
+      
       <p>É um projeto feito com o <strong>React.js</strong> no front-end e <strong>Firebase</strong> no back-end</p>
+      
         <br />
+      
       <p>Fiz esse site com o intuito de aprender sobre essas tecnologias</p>
+      
+      <br />
+
+      <Link className="btn" to="/create/post">
+        Crie um post!
+      </Link>
     </div>
   )
 }
--- src/pages/NewPost/NewPost.jsx
+++ src/pages/NewPost/NewPost.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import "../Registration/Registration.css"
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    
+      
+      <form>
+        <h2>Compartilhe momentos!</h2>
+        <div className="fields">
+          <label>
+            Título da postagem
+            <input type="text" maxLength={50} />
+          </label>          
+        </div>
+        <div className="fields">
+          <label>
+            Link da imagem
+            <input type="text" />
+          </label>
+        </div>
+        <div className="fields">
+          <label>
+            Legenda
+            <input type="text" maxLength={150} />
+          </label>
+        </div>
+        <div className="fields">
+          <input type="submit" value="publicar" />
+        </div>
+      </form>
+    
   )
 }
 
